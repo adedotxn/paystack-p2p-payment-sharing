248918a13fc8f15a006ded0827ad5f2344d0f0a0
adedotxn <adedotxn.dev@gmail.com>
2024-10-10 10:39:58 +0100

    chore: fixed card not having enough space for big numbers

--- client/src/pages/dashboard/root.tsx
+++ client/src/pages/dashboard/root.tsx
@@ -48,7 +48,7 @@ export default function Dashboard() {
               Welcome back, {user?.name.split(" ")[0] ?? ""}!
             </h1>
 
-            <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+            <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-2">
               <QuickActionsCard />
               <BalanceSummaryCard />
               <ActiveBillsCard />
